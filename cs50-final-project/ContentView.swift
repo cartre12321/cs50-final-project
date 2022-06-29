9568b86cc0a61d0a7c598785dc699adbac320fcf
Carter Riddall <carter.riddall@outlook.com>
2022-06-29 12:09:22 -0400

    Initial Commit

---
+++ cs50-final-project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cs50-final-project
+//
+//  Created by Carter Riddall on 2022-06-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cs50-final-project/cs50_final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs50_final_projectApp.swift
+//  cs50-final-project
+//
+//  Created by Carter Riddall on 2022-06-29.
+//
+
+import SwiftUI
+
+@main
+struct cs50_final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
